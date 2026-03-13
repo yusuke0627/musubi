b30065e4a00c51e7616bdfea449c44d0b24ad626
yusuke0627 <arakaki.yusuke0627@gmail.com>
2026-03-13 21:09:30 +0900

    fix: ダッシュボードページを強制的に動的レンダリング (Dynamic Rendering) に変更

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -4,6 +4,8 @@ import Link from "next/link";
 import StatsChart from "@/components/StatsChart";
 import { processClicks, completePayout, updateRevShare, reviewAd } from "./actions";
 
+export const dynamic = "force-dynamic";
+
 export default async function AdminDashboard() {
   const stats = db.prepare(`
     SELECT 
